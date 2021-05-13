5d08ca6d3f8473398db0b5e0b6b45f0829baa827
pryvvid <pryvvid@gmail.com>
2021-05-13 14:39:23 +0300

    feat: user rest service

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,35 @@
+let userDB = [];
+
 const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+  const DB = await userDB;
+  return DB;
 };
 
-module.exports = { getAll };
+const getOne = async (id) => {
+  let user = null;
+   try {
+     user = await userDB.find((u) => u.id === id)     
+   } catch (e) {
+     process.stderr.write(e)
+   }
+   return user
+};
+
+const createUser = async (user) => {
+  await userDB.push(user)
+};
+
+const updateUser = async (id, newUserInfo) => {
+  const userIndex = await userDB.findIndex(user => user.id === id);
+  const updatedUser = {
+    ...userDB[userIndex],
+    ...newUserInfo
+  }
+  userDB[userIndex] = updatedUser;
+};
+
+const deleteUser = async (id) => {
+   userDB = await userDB.filter((user) => user.id !== id)
+};
+
+module.exports = { getAll, getOne, createUser, updateUser, deleteUser };
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,33 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+router.route('/:id').get(async (req, res) => {
+  const { id } = req.params;
+  const user = await usersService.getOne(id);    
+  res.status(200).json(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+  const { name, login, password } = req.body;
+  const newUser = new User({name, login, password})
+  await usersService.createUser(newUser)
+  res.status(201).json(User.toResponse(newUser))
+})
+
+router.route('/:id').put(async (req, res) => {
+  const { id } = req.params;
+  const { name, login, password } = req.body;
+  const updatedUser = {
+    name, login, password
+  }
+  await usersService.updateUser(id, updatedUser);
+  res.status(200).json(updatedUser);
+});
+
+router.route('/:id').delete(async (req, res) => {
+  const { id } = req.params;
+  await usersService.deleteUser(id);
+  res.status(204).json({ message: 'user deleted' });
+});
+
 module.exports = router;
