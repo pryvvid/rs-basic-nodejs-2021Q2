210804bf864cc1108c4bea918659c46ce96e37a7
pryvvid <pryvvid@gmail.com>
2021-05-15 13:19:50 +0300

    fix: new task model

--- src/resources/tasks/task.model.js
+++ src/resources/tasks/task.model.js
@@ -6,9 +6,9 @@ class Task {
     title = 'New task',
     order = 0,
     description = 'New task',
-    userId = "",
-    boardId = "",
-    columnId = ""
+    userId = null,
+    boardId = null,
+    columnId = null,
   } = {}) {
     this.id = id;
     this.title = title;
