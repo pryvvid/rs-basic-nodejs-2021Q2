a5536b526d15b5918d61a919a0477feaff1068be
pryvvid <pryvvid@gmail.com>
2021-05-15 11:06:04 +0300

    fix: routes

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -2,69 +2,71 @@ const router = require('express').Router({mergeParams: true});
 const Task = require('./task.model');
 const tasksService = require('./task.service');
 
-router.route('/').get(async (req, res) => {
-  const tasks = await tasksService.getAll();
-  // map task fields to exclude secret fields like "password"
-  res.json(tasks.map(Task.toResponse));
-});
+router.route('/')
 
-router.route('/:id').get(async (req, res) => {
-  const { id } = req.params;
-  const task = await tasksService.getOne(id);
-  if (task === undefined) {
-    res.status(404).json({error: "Not found"});
-  }     
-  res.status(200).json(Task.toResponse(task));
-});
-
-router.route('/').post(async (req, res) => {
-  const { boardId } = req.params;
-  const { 
-    title,
-    order,
-    description,
-    userId,
-    columnId
-  } = req.body;
-
-  const newTask = new Task({
-    title,
-    order,
-    description,
-    userId,
-    boardId,
-    columnId
+  .get(async (req, res) => {
+    const tasks = await tasksService.getAll();
+    res.json(tasks.map(Task.toResponse));
   })
-  await tasksService.createTask(newTask)
-  res.status(201).json(Task.toResponse(newTask))
-})
 
-router.route('/:id').put(async (req, res) => {
-  const { id } = req.params;
-  const {     
-    title,
-    order,
-    description,
-    userId,
-    boardId,
-    columnId
-  } = req.body;
-  const updatedTask = {
-    title,
-    order,
-    description,
-    userId,
-    boardId,
-    columnId
-  }
-  await tasksService.updateTask(id, updatedTask);
-  res.status(200).json(updatedTask);
-});
+  .post(async (req, res) => {
+    const { boardId } = req.params;
+    const { 
+      title,
+      order,
+      description,
+      userId,
+      columnId
+    } = req.body;  
+    const newTask = new Task({
+      title,
+      order,
+      description,
+      userId,
+      boardId,
+      columnId
+    })
+    await tasksService.createTask(newTask)
+    res.status(201).json(Task.toResponse(newTask))
+  });
 
-router.route('/:id').delete(async (req, res) => {
-  const { id } = req.params;
-  await tasksService.deleteTask(id);
-  res.status(204).send('Deleted');
-});
+router.route('/:id')
+
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const task = await tasksService.getOne(id);
+    if (!task) {
+      res.status(404).json({ error: "Not found" });
+    }     
+    res.status(200).json(Task.toResponse(task));
+  })
+
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const {     
+      title,
+      order,
+      description,
+      userId,
+      boardId,
+      columnId
+    } = req.body;
+    const updatedTask = {
+      title,
+      order,
+      description,
+      userId,
+      boardId,
+      columnId
+    }
+    await tasksService.updateTask(id, updatedTask);
+    res.status(200).json(updatedTask);
+  })
+
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    await tasksService.deleteTask(id);
+    res.status(204).json({ message: 'Task was deleted' });
+  });
 
 module.exports = router;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -2,39 +2,50 @@ const router = require('express').Router();
 const User = require('./user.model');
 const usersService = require('./user.service');
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+router
+  .route('/')
 
-router.route('/:id').get(async (req, res) => {
-  const { id } = req.params;
-  const user = await usersService.getOne(id);    
-  res.status(200).json(User.toResponse(user));
-});
+  .get(async (req, res) => {
+    const users = await usersService.getAll();
+    // map user fields to exclude secret fields like "password"
+    res.json(users.map(User.toResponse));
+  })
 
-router.route('/').post(async (req, res) => {
-  const { name, login, password } = req.body;
-  const newUser = new User({name, login, password})
-  await usersService.createUser(newUser)
-  res.status(201).json(User.toResponse(newUser))
-})
+  .post(async (req, res) => {
+    const { name, login, password } = req.body;
+    const newUser = new User({ name, login, password });
+    await usersService.createUser(newUser);
+    res.status(201).json(User.toResponse(newUser));
+  });
 
-router.route('/:id').put(async (req, res) => {
-  const { id } = req.params;
-  const { name, login, password } = req.body;
-  const updatedUser = {
-    name, login, password
-  }
-  await usersService.updateUser(id, updatedUser);
-  res.status(200).json(updatedUser);
-});
+router
+  .route('/:id')
 
-router.route('/:id').delete(async (req, res) => {
-  const { id } = req.params;
-  await usersService.deleteUser(id);
-  res.status(204).json({ message: 'user deleted' });
-});
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const user = await usersService.getOne(id);
+    if (!user) {
+      res.status(404).json({ error: 'Not found' });
+    }
+    res.status(200).json(User.toResponse(user));
+  })
+
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { name, login, password } = req.body;
+    const updatedUser = {
+      name,
+      login,
+      password,
+    };
+    await usersService.updateUser(id, updatedUser);
+    res.status(200).json(updatedUser);
+  })
+
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    await usersService.deleteUser(id);
+    res.status(204).json({ message: 'User was deleted' });
+  });
 
 module.exports = router;
