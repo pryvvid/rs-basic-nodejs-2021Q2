0457fd1efd164a015cb30804b1543b684aed16b5
pryvvid <pryvvid@gmail.com>
2021-06-18 13:06:05 +0300

    feat: Board entity

---
+++ src/entity/Board.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+class Board {
+
+  @PrimaryGeneratedColumn("uuid")
+  id?: string;
+
+  @Column()
+  title: string;
+
+  @Column({ type: "json", nullable: true })
+  columns?: string;
+}
+
+export { Board }
