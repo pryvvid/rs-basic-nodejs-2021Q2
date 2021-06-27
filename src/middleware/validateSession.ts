1ebc41bdb000e927db31ecbd13a6e5b8e95579a4
pryvvid <pryvvid@gmail.com>
2021-06-27 15:42:16 +0300

    feat: validateToken util

--- src/middleware/validateSession.ts
+++ src/middleware/validateSession.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { validateToken } from '../utils/validateToken'
 
 const validateSession = async (req: Request, res: Response, next: NextFunction) => {
   if (req.path === "/") {
@@ -13,6 +14,8 @@ const validateSession = async (req: Request, res: Response, next: NextFunction) 
   const sessionToken = req.headers.authorization;
   // console.log(sessionToken);
   if (!sessionToken) return res.status(401).send({ message: "No token provided." });
+  const isValid = validateToken(sessionToken);
+  if (isValid === false) return res.status(401).send({ message: "Authentication must follow Bearer scheme." });
   return next();
 }
 
